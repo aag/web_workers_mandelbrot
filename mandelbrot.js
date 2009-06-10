1ae9799f95ba3ee37f573b888195ce4258b95ce5
Adam Goforth <adam@Fire.(none)>
2009-06-09 20:21:06 -0400

    Code cleanup

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,27 +1,30 @@
 var startTime;
 var slicesFinished = 0;
 var numWorkers = 0;
+var returnedValueArrays = null;
 
 function draw() {
 	var canvas = document.getElementById("can");
 	if (canvas.getContext) {
 		var ctx = canvas.getContext("2d");
 
-        var imageWidth = canvas.width;
+		var imageWidth = canvas.width;
 		var imageHeight = canvas.height;
 
-        numWorkers = 2;
-        slicesFinished = 0;
-        
+		numWorkers = 6;
+		slicesFinished = 0;
+		returnedValueArrays = new Array(numWorkers);
+		
 		// We have all the info we need, so do the drawing
-        startTime = new Date();
+		startTime = new Date();
+		logToMessageDiv("Starting to draw: " + startTime);
 		drawMandelbrot(ctx, imageWidth, imageHeight, numWorkers);
 	}
 }
 
 function drawMandelbrot(ctx, width, height, numWorkers) {
 	var maxIterations = 100;
-    var minReal = -1.0;
+	var minReal = -1.0;
 	var maxReal = 0.2;
 	var minImaginary = -1.2;
 
@@ -30,63 +33,86 @@ function drawMandelbrot(ctx, width, height, numWorkers) {
 		var realFactor = (maxReal - minReal) / (width - 1);
 		var imaginaryFactor = (maxImaginary - minImaginary) / (height - 1);
 		
-        var startX = 0;
-        var startY = 0;
-        
-        var sliceForEachWorker = parseInt(height / numWorkers);
-        
-        // Start up the workers
-        for (var i = 0; i < numWorkers; i++) {
-            startY = parseInt(i * sliceForEachWorker);
-            
-            var argArray = [startX, width, startY, sliceForEachWorker, maxImaginary, imaginaryFactor, minReal, realFactor, maxIterations];
-            
-            var worker = new Worker("mandelbrotRenderer.js");
-            
-            worker.onmessage = function(event) {
-                var retArray = event.data;
-                var retStartX = retArray[0];
-                var retStartY = retArray[1];
-                var valueArray = retArray[2];
-                
-                paintToCanvas(ctx, retStartX, retStartY, valueArray);
-            };
+		var startX = 0;
+		var startY = 0;
+		
+		var sliceForEachWorker = parseInt(height / numWorkers);
+		
+		// Start up the workers
+		for (var i = 0; i < numWorkers; i++) {
+			startY = parseInt(i * sliceForEachWorker);
+			
+			var args = new Object;
+			args.workerID = i;
+			args.startX = startX;
+			args.width = width;
+			args.startY = startY;
+			args.height = sliceForEachWorker;
+			args.maxImaginary = maxImaginary;
+			args.imaginaryFactor = imaginaryFactor;
+			args.minReal = minReal;
+			args.realFactor = realFactor;
+			args.maxIterations = maxIterations;
 
-            worker.onerror = function(error) {
-                dump("Worker error: " + error.message + "\n");
-                throw error;
-            };
+			var worker = new Worker("mandelbrotRenderer.js");
+			
+			worker.onmessage = function(event) {
+				var theTime = new Date();
+				var renderValues = event.data;
+				var workerID = renderValues.workerID;
+				var retStartX = renderValues.startX;
+				var retStartY = renderValues.startY;
+				var valueArray = renderValues.valueArray;
 
-            worker.postMessage(argArray);
-        }
+				logToMessageDiv("Worker " + workerID + " returned: " + theTime + " diff: " + ((theTime - startTime) / 1000));
+				
+				paintToCanvas(ctx, retStartX, retStartY, valueArray);
+			};
+
+			worker.onerror = function(error) {
+				dump("Worker error: " + error.message + "\n");
+				throw error;
+			};
+
+			worker.postMessage(args);
+		}
 	}
 }
 
 function paintToCanvas(ctx, startX, startY, array) {
-    var arrayHeight = array.length;
-    var arrayWidth = 0;
-    
-    if (arrayHeight > 0) {
-        arrayWidth = array[0].length;
-    }
+	var arrayHeight = array.length;
+	var arrayWidth = 0;
+	
+	if (arrayHeight > 0) {
+		arrayWidth = array[0].length;
+	}
 
-    var lastBrightness = 0;
-    for (var i = 0; i < arrayHeight; i++) {
-        for (var j = startX; j < arrayWidth; j++) {
-            var brightness = parseInt((array[i][j] / 100) * 254);
-            if (lastBrightness != brightness) {
-                ctx.fillStyle = "rgba(" + brightness + ", 0, 0, 1.0)";
-                lastBrightness = brightness;
-            }
-            ctx.fillRect(j, (startY + i), 1, 1);
-        }
-    }
-    
-    slicesFinished = slicesFinished + 1;
-    
-    if (slicesFinished == numWorkers) {
-        var endTime = new Date();
-        var timing = (endTime - startTime) / 1000;
-        document.getElementById("messageDiv").textContent = "Rendering took " + timing + " seconds.";
-    }
+	var lastBrightness = 0;
+	for (var i = 0; i < arrayHeight; i++) {
+		for (var j = startX; j < arrayWidth; j++) {
+			var brightness = parseInt((array[i][j] / 100) * 254);
+			if (lastBrightness != brightness) {
+				ctx.fillStyle = "rgba(" + brightness + ", 0, 0, 1.0)";
+				lastBrightness = brightness;
+			}
+			ctx.fillRect(j, (startY + i), 1, 1);
+		}
+	}
+	
+	slicesFinished = slicesFinished + 1;
+	
+	if (slicesFinished == numWorkers) {
+		var endTime = new Date();
+		var timing = (endTime - startTime) / 1000;
+		logToMessageDiv("Rendering took " + timing + " seconds.");
+	}
+}
+
+var messageDiv;
+function logToMessageDiv(message) {
+	if (!messageDiv) {
+		messageDiv = document.getElementById("messageDiv");
+	}
+
+	messageDiv.innerHTML += message + "<br />\n";
 }
--- mandelbrotRenderer.js
+++ mandelbrotRenderer.js
@@ -1,71 +1,73 @@
 onmessage = function(event) {
-    var argArray = event.data;
-    
-    var retArray = renderRows(argArray);
-    
-    postMessage(retArray);
+	var argArray = event.data;
+	
+	var retArray = renderRows(argArray);
+	
+	postMessage(retArray);
 };
 
-function renderRows(argArray) {
-    var startX = argArray[0];
-    var width = argArray[1];
-    var startY = argArray[2];
-    var height = argArray[3];
-    var maxImaginary = argArray[4];
-    var imaginaryFactor = argArray[5];
-    var minReal = argArray[6];
-    var realFactor = argArray[7];
-    var maxIterations = argArray[8];
-    
-    var valueArray = new Array(height);
-    
-    for (var y = 0; y < height; y++) {
-        var cImaginary = maxImaginary - (startY + y) * imaginaryFactor;
+function renderRows(args) {
+	var workerID = args.workerID;
+	var startX = args.startX;
+	var width = args.width;
+	var startY = args.startY;
+	var height = args.height;
+	var maxImaginary = args.maxImaginary;
+	var imaginaryFactor = args.imaginaryFactor;
+	var minReal = args.minReal;
+	var realFactor = args.realFactor;
+	var maxIterations = args.maxIterations;
+	
+	var valueArray = new Array(height);
+	
+	for (var y = 0; y < height; y++) {
+		var cImaginary = maxImaginary - (startY + y) * imaginaryFactor;
 
-        var rowArray = renderRow(startX, width, minReal, realFactor, cImaginary, maxIterations);
-        valueArray[y] = rowArray;
-    }
-    
-    var retArray = new Array(3);
-    retArray[0] = startX;
-    retArray[1] = startY;
-    retArray[2] = valueArray;
-    
-    return retArray;
+		var rowArray = renderRow(startX, width, minReal, realFactor, cImaginary, maxIterations);
+		valueArray[y] = rowArray;
+	}
+	
+	var retValues = new Object;
+	retValues.workerID = workerID;
+	retValues.startX = startX;
+	retValues.startY = startY;
+	retValues.valueArray = valueArray;
+
+	return retValues;
 }
 
 function renderRow(startX, width, minReal, realFactor, cImaginary, maxIterations) {
-    var retArray = new Array(width);
+	var retArray = new Array(width);
 
-    for (var x = startX; x < width; x++) {
-        var cReal = minReal + x * realFactor;
+	for (var x = startX; x < width; x++) {
+		var cReal = minReal + x * realFactor;
 
-        // Calculate if c is in the set
-        var zReal = cReal;
-        var zImaginary = cImaginary;
-        var inSet = true;
-        var it = 0;
+		// Calculate if c is in the set
+		var zReal = cReal;
+		var zImaginary = cImaginary;
+		var inSet = true;
+		var it = 0;
 
-        for (it = 0; it < maxIterations; ++it) {
-            var zReal2 = zReal * zReal;
-            var zImaginary2 = zImaginary * zImaginary;
+		for (it = 0; it < maxIterations; ++it) {
+			var zReal2 = zReal * zReal;
+			var zImaginary2 = zImaginary * zImaginary;
 
-            // if abs(z) > 2
-            if ((zReal2 + zImaginary2) > 4) {
-                inSet = false;
-                break;
-            }
-            zImaginary = 2 * zReal * zImaginary + cImaginary;
-            zReal = zReal2 - zImaginary2 + cReal;
-        }
+			// if abs(z) > 2
+			if ((zReal2 + zImaginary2) > 4) {
+				inSet = false;
+				break;
+			}
+			zImaginary = 2 * zReal * zImaginary + cImaginary;
+			zReal = zReal2 - zImaginary2 + cReal;
+		}
 
-        if (inSet) {
-            retArray[x] = 0;
-        } else {
-            // Scale the brightness to 100
-            retArray[x] = (it / maxIterations) * 100;
-        }
-    }
-    
-    return retArray;
+		if (inSet) {
+			retArray[x] = 0;
+		} else {
+			// Scale the brightness to 100
+			retArray[x] = (it / maxIterations) * 100;
+		}
+	}
+	
+	return retArray;
 }
