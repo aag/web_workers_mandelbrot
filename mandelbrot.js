305b6c0808e96eb965c26e59944d27a1cc154d9e
Adam Goforth <tennisbike@hotmail.com>
2011-12-30 22:21:43 -0500

    Convert tabs to spaces.

--- mandelbrot.js
+++ mandelbrot.js
@@ -3,152 +3,152 @@ var slicesFinished = 0;
 var workerPool = null;
 
 function init() {
-	reInitWorkerPool();
+    reInitWorkerPool();
 }
 
 function reInitWorkerPool() {
-	var numWorkers = getNumWorkersFromPage();
-	logToMessageDiv("Re-initializing the worker pool to size: " + numWorkers);
-	workerPool = null;
-	workerPool = new Array(numWorkers);
+    var numWorkers = getNumWorkersFromPage();
+    logToMessageDiv("Re-initializing the worker pool to size: " + numWorkers);
+    workerPool = null;
+    workerPool = new Array(numWorkers);
 
-	for (var i = 0; i < workerPool.length; i++) {
-		workerPool[i] = getNewWorker();
-	}
+    for (var i = 0; i < workerPool.length; i++) {
+        workerPool[i] = getNewWorker();
+    }
 }
 
 function getNewWorker() {
-	var worker = new Worker("mandelbrotRenderer.js");
-			
-	worker.onmessage = function(event) {
-		var theTime = new Date();
+    var worker = new Worker("mandelbrotRenderer.js");
+            
+    worker.onmessage = function(event) {
+        var theTime = new Date();
 
-		var renderValues = event.data;
-		var retWorkerID = renderValues.workerID;
-		var retStartX = renderValues.startX;
-		var retStartY = renderValues.startY;
-		var retImageData = renderValues.imageData;
+        var renderValues = event.data;
+        var retWorkerID = renderValues.workerID;
+        var retStartX = renderValues.startX;
+        var retStartY = renderValues.startY;
+        var retImageData = renderValues.imageData;
 
-		logToMessageDiv("Worker " + retWorkerID + " returned from rendering: " + theTime + " diff: " + ((theTime - startTime) / 1000));
+        logToMessageDiv("Worker " + retWorkerID + " returned from rendering: " + theTime + " diff: " + ((theTime - startTime) / 1000));
 
-		var width = getCanvasContext().canvas.width;
-		var height = getCanvasContext().canvas.height;
-		var sliceForEachWorker = parseInt(height / workerPool.length);
+        var width = getCanvasContext().canvas.width;
+        var height = getCanvasContext().canvas.height;
+        var sliceForEachWorker = parseInt(height / workerPool.length);
 
-		var newImageData = getCanvasContext().createImageData(width, sliceForEachWorker);
+        var newImageData = getCanvasContext().createImageData(width, sliceForEachWorker);
         for (var i = 0; i < retImageData.length; i++) {
             newImageData.data[i] = retImageData[i];
         }
-		getCanvasContext().putImageData(newImageData, retStartX, retStartY);
+        getCanvasContext().putImageData(newImageData, retStartX, retStartY);
 
-		theTime = new Date();
+        theTime = new Date();
         var elapsedTime = ((theTime - startTime) / 1000);
-		logToMessageDiv("Worker " + retWorkerID + " finished drawing: " + theTime + " diff: " + elapsedTime);
+        logToMessageDiv("Worker " + retWorkerID + " finished drawing: " + theTime + " diff: " + elapsedTime);
 
         slicesFinished++;
         if (slicesFinished == workerPool.length) {
             logToMessageDiv("All workers finished.  Total time elapsed: " + elapsedTime);
             logToTimeTable(workerPool.length, elapsedTime);
         }
-	};
+    };
 
-	worker.onerror = function(error) {
-		logToMessageDiv("Worker error: " + error.message + "\n");
-		throw error;
-	};
+    worker.onerror = function(error) {
+        logToMessageDiv("Worker error: " + error.message + "\n");
+        throw error;
+    };
 
-	return worker;
+    return worker;
 }
 
 function draw() {
-	var imageWidth = getCanvasContext().canvas.width;
-	var imageHeight = getCanvasContext().canvas.height;
+    var imageWidth = getCanvasContext().canvas.width;
+    var imageHeight = getCanvasContext().canvas.height;
 
-	// Clear the whole canvas
-	getCanvasContext().clearRect(0, 0, imageWidth, imageHeight);
+    // Clear the whole canvas
+    getCanvasContext().clearRect(0, 0, imageWidth, imageHeight);
 
-	slicesFinished = 0;
-		
-	// We have all the info we need, so do the drawing
-	startTime = new Date();
-	logToMessageDiv("Starting the worker threads.");
-	drawMandelbrotWithWorkers();
+    slicesFinished = 0;
+        
+    // We have all the info we need, so do the drawing
+    startTime = new Date();
+    logToMessageDiv("Starting the worker threads.");
+    drawMandelbrotWithWorkers();
 }
 
 function getNumWorkersFromPage() {
-	var retVal = 0;
+    var retVal = 0;
 
-	var nwSelect = document.getElementById("numWorkers");
-	if (nwSelect) {
-		retVal = parseInt(nwSelect.options[nwSelect.selectedIndex].value);
-	}
+    var nwSelect = document.getElementById("numWorkers");
+    if (nwSelect) {
+        retVal = parseInt(nwSelect.options[nwSelect.selectedIndex].value);
+    }
 
-	return retVal;
+    return retVal;
 }
 
 function drawMandelbrotWithWorkers() {
-	var maxIterations = 100;
-	var minReal = -2.0;
-	var maxReal = 1.0;
-	var minImaginary = -1.2;
+    var maxIterations = 100;
+    var minReal = -2.0;
+    var maxReal = 1.0;
+    var minImaginary = -1.2;
 
-	var width = getCanvasContext().canvas.width;
-	var height = getCanvasContext().canvas.height;
+    var width = getCanvasContext().canvas.width;
+    var height = getCanvasContext().canvas.height;
 
-	if (getCanvasContext()) {
-		var maxImaginary = minImaginary + (maxReal - minReal) * (height / width);
-		var realFactor = (maxReal - minReal) / (width - 1);
-		var imaginaryFactor = (maxImaginary - minImaginary) / (height - 1);
-		
-		var startX = 0;
-		var startY = 0;
-		
-		var sliceForEachWorker = parseInt(height / workerPool.length);
-		
-		// Send work to the workers
-		for (var i = 0; i < workerPool.length; i++) {
-			startY = parseInt(i * sliceForEachWorker);
+    if (getCanvasContext()) {
+        var maxImaginary = minImaginary + (maxReal - minReal) * (height / width);
+        var realFactor = (maxReal - minReal) / (width - 1);
+        var imaginaryFactor = (maxImaginary - minImaginary) / (height - 1);
+        
+        var startX = 0;
+        var startY = 0;
+        
+        var sliceForEachWorker = parseInt(height / workerPool.length);
+        
+        // Send work to the workers
+        for (var i = 0; i < workerPool.length; i++) {
+            startY = parseInt(i * sliceForEachWorker);
 
-			var imageData = getCanvasContext().createImageData(width, sliceForEachWorker);
-			
-			var args = new Object;
-			args.workerID = i;
-			args.startX = startX;
-			args.width = width;
-			args.startY = startY;
-			args.height = sliceForEachWorker;
-			args.maxImaginary = maxImaginary;
-			args.imaginaryFactor = imaginaryFactor;
-			args.minReal = minReal;
-			args.realFactor = realFactor;
-			args.maxIterations = maxIterations;
-			args.imageData = imageData.data;
+            var imageData = getCanvasContext().createImageData(width, sliceForEachWorker);
+            
+            var args = new Object;
+            args.workerID = i;
+            args.startX = startX;
+            args.width = width;
+            args.startY = startY;
+            args.height = sliceForEachWorker;
+            args.maxImaginary = maxImaginary;
+            args.imaginaryFactor = imaginaryFactor;
+            args.minReal = minReal;
+            args.realFactor = realFactor;
+            args.maxIterations = maxIterations;
+            args.imageData = imageData.data;
 
-			workerPool[i].postMessage(args);
-		}
-	}
+            workerPool[i].postMessage(args);
+        }
+    }
 }
 
 var ctx;
 function getCanvasContext() {
-	if (!ctx) {
-		var canvas = document.getElementById("can");
-		if (canvas.getContext) {
-			ctx = canvas.getContext("2d");
-		}
-	}	
+    if (!ctx) {
+        var canvas = document.getElementById("can");
+        if (canvas.getContext) {
+            ctx = canvas.getContext("2d");
+        }
+    }   
 
-	return ctx;
+    return ctx;
 }
 
 var messageDiv;
 function logToMessageDiv(message) {
     return;
-	if (!messageDiv) {
-		messageDiv = document.getElementById("messageDiv");
-	}
+    if (!messageDiv) {
+        messageDiv = document.getElementById("messageDiv");
+    }
 
-	messageDiv.innerHTML += message + "<br />\n";
+    messageDiv.innerHTML += message + "<br />\n";
 }
 
 function logToTimeTable(workers, time) {
--- mandelbrotRenderer.js
+++ mandelbrotRenderer.js
@@ -1,69 +1,69 @@
 onmessage = function(event) {
-	var args = event.data;
-	
-	var retArray = renderRows(args);
-	
-	postMessage(retArray);
+    var args = event.data;
+    
+    var retArray = renderRows(args);
+    
+    postMessage(retArray);
 };
 
 function renderRows(args) {
-	var workerID = args.workerID;
-	var startX = args.startX;
-	var width = args.width;
-	var startY = args.startY;
-	var height = args.height;
-	var maxImaginary = args.maxImaginary;
-	var imaginaryFactor = args.imaginaryFactor;
-	var minReal = args.minReal;
-	var realFactor = args.realFactor;
-	var maxIterations = args.maxIterations;
-	var imageData = args.imageData;
-	
-	for (var y = 0; y < height; y++) {
-		var cImaginary = maxImaginary - (startY + y) * imaginaryFactor;
+    var workerID = args.workerID;
+    var startX = args.startX;
+    var width = args.width;
+    var startY = args.startY;
+    var height = args.height;
+    var maxImaginary = args.maxImaginary;
+    var imaginaryFactor = args.imaginaryFactor;
+    var minReal = args.minReal;
+    var realFactor = args.realFactor;
+    var maxIterations = args.maxIterations;
+    var imageData = args.imageData;
+    
+    for (var y = 0; y < height; y++) {
+        var cImaginary = maxImaginary - (startY + y) * imaginaryFactor;
 
-		renderRow(startX, y, width, minReal, realFactor, cImaginary, maxIterations, imageData);
-	}
-	
-	var retValues = new Object;
-	retValues.workerID = workerID;
-	retValues.startX = startX;
-	retValues.startY = startY;
-	retValues.imageData = imageData;
+        renderRow(startX, y, width, minReal, realFactor, cImaginary, maxIterations, imageData);
+    }
+    
+    var retValues = new Object;
+    retValues.workerID = workerID;
+    retValues.startX = startX;
+    retValues.startY = startY;
+    retValues.imageData = imageData;
 
-	return retValues;
+    return retValues;
 }
 
 function renderRow(startX, y, width, minReal, realFactor, cImaginary, maxIterations, imageData) {
-	for (var x = startX; x < width; x++) {
-		var cReal = minReal + x * realFactor;
+    for (var x = startX; x < width; x++) {
+        var cReal = minReal + x * realFactor;
 
-		// Calculate if c is in the set
-		var zReal = cReal;
-		var zImaginary = cImaginary;
-		var inSet = true;
-		var it = 0;
+        // Calculate if c is in the set
+        var zReal = cReal;
+        var zImaginary = cImaginary;
+        var inSet = true;
+        var it = 0;
 
-		for (it = 0; it < maxIterations; ++it) {
-			var zReal2 = zReal * zReal;
-			var zImaginary2 = zImaginary * zImaginary;
+        for (it = 0; it < maxIterations; ++it) {
+            var zReal2 = zReal * zReal;
+            var zImaginary2 = zImaginary * zImaginary;
 
-			// if abs(z) > 2
-			if ((zReal2 + zImaginary2) > 4) {
-				inSet = false;
-				break;
-			}
-			zImaginary = 2 * zReal * zImaginary + cImaginary;
-			zReal = zReal2 - zImaginary2 + cReal;
-		}
+            // if abs(z) > 2
+            if ((zReal2 + zImaginary2) > 4) {
+                inSet = false;
+                break;
+            }
+            zImaginary = 2 * zReal * zImaginary + cImaginary;
+            zReal = zReal2 - zImaginary2 + cReal;
+        }
 
-		var intensity = 0;
+        var intensity = 0;
         var red = 0;
         var green = 0;
         var blue = 0;
 
-		if (!inSet) {
-			intensity = ((it / maxIterations) * 722) + 40;
+        if (!inSet) {
+            intensity = ((it / maxIterations) * 722) + 40;
             if (intensity > 0 && intensity <= 254) {
                 red = intensity;
             } else if (intensity > 254 && intensity <= 508) {
@@ -74,14 +74,14 @@ function renderRow(startX, y, width, minReal, realFactor, cImaginary, maxIterati
                 green = 254;
                 blue = intensity - 508;
             }
-		}
+        }
 
-		var pixelStartPos = ((y * width) + x) * 4;
-		// Set the opacity to solid
-		imageData[pixelStartPos + 3] = 255;
-		// Set the color
-		imageData[pixelStartPos] = red;
-		imageData[pixelStartPos + 1] = green;
-		imageData[pixelStartPos + 2] = blue;
-	}
+        var pixelStartPos = ((y * width) + x) * 4;
+        // Set the opacity to solid
+        imageData[pixelStartPos + 3] = 255;
+        // Set the color
+        imageData[pixelStartPos] = red;
+        imageData[pixelStartPos + 1] = green;
+        imageData[pixelStartPos + 2] = blue;
+    }
 }
