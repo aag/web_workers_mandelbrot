81e3b3010dab4dcd41f20e86b9900de6d4716acf
Adam Goforth <tennisbike@hotmail.com>
2011-12-30 20:37:11 -0500

    Refactor to use a worker pool instead of creating workers at render time.

--- mandelbrot.js
+++ mandelbrot.js
@@ -2,27 +2,74 @@ var startTime;
 var slicesFinished = 0;
 var numWorkers = 0;
 var returnedValueArrays = null;
+var workerPool = null;
+
+function init() {
+	reInitWorkerPool();
+}
+
+function reInitWorkerPool() {
+	numWorkers = getNumWorkersFromPage();
+	logToMessageDiv("Re-initializing the worker pool to size: " + numWorkers);
+	workerPool = null;
+	workerPool = new Array(numWorkers);
+
+	for (var i = 0; i < workerPool.length; i++) {
+		workerPool[i] = getNewWorker();
+	}
+}
+
+function getNewWorker() {
+	var worker = new Worker("mandelbrotRenderer.js");
+			
+	worker.onmessage = function(event) {
+		var theTime = new Date();
+
+		var renderValues = event.data;
+		var retWorkerID = renderValues.workerID;
+		var retStartX = renderValues.startX;
+		var retStartY = renderValues.startY;
+		var retImageData = renderValues.imageData;
+
+		logToMessageDiv("Worker " + retWorkerID + " returned from rendering: " + theTime + " diff: " + ((theTime - startTime) / 1000));
+
+		var width = getCanvasContext().canvas.width;
+		var height = getCanvasContext().canvas.height;
+		var sliceForEachWorker = parseInt(height / numWorkers);
+
+		var newImageData = getCanvasContext().createImageData(width, sliceForEachWorker);
+        for (var i = 0; i < retImageData.length; i++) {
+            newImageData.data[i] = retImageData[i];
+        }
+		getCanvasContext().putImageData(newImageData, retStartX, retStartY);
+
+		theTime = new Date();
+		logToMessageDiv("Worker " + retWorkerID + " finished drawing: " + theTime + " diff: " + ((theTime - startTime) / 1000));
+	};
+
+	worker.onerror = function(error) {
+		logToMessageDiv("Worker error: " + error.message + "\n");
+		throw error;
+	};
+
+	return worker;
+}
 
 function draw() {
-	var canvas = document.getElementById("can");
-	if (canvas.getContext) {
-		var ctx = canvas.getContext("2d");
+	var imageWidth = getCanvasContext().canvas.width;
+	var imageHeight = getCanvasContext().canvas.height;
 
-		var imageWidth = canvas.width;
-		var imageHeight = canvas.height;
+	// Clear the whole canvas
+	getCanvasContext().clearRect(0, 0, imageWidth, imageHeight);
 
-		// Clear the whole canvas
-		ctx.clearRect(0, 0, imageWidth, imageHeight);
-
-		numWorkers = getNumWorkersFromPage();
-		slicesFinished = 0;
-		returnedValueArrays = new Array(numWorkers);
+	numWorkers = getNumWorkersFromPage();
+	slicesFinished = 0;
+	returnedValueArrays = new Array(numWorkers);
 		
-		// We have all the info we need, so do the drawing
-		startTime = new Date();
-		logToMessageDiv("Starting to draw: " + startTime);
-		drawMandelbrotWithWorkers(ctx, imageWidth, imageHeight, numWorkers);
-	}
+	// We have all the info we need, so do the drawing
+	startTime = new Date();
+	logToMessageDiv("Starting to draw: " + startTime);
+	drawMandelbrotWithWorkers(numWorkers);
 }
 
 function getNumWorkersFromPage() {
@@ -33,18 +80,19 @@ function getNumWorkersFromPage() {
 		retVal = parseInt(nwSelect.options[nwSelect.selectedIndex].value);
 	}
 
-	logToMessageDiv("NumWorkers: " + retVal);
-
 	return retVal;
 }
 
-function drawMandelbrotWithWorkers(ctx, width, height, numWorkers) {
+function drawMandelbrotWithWorkers(numWorkers) {
 	var maxIterations = 100;
 	var minReal = -2.0;
 	var maxReal = 1.0;
 	var minImaginary = -1.2;
 
-	if (ctx) {
+	var width = getCanvasContext().canvas.width;
+	var height = getCanvasContext().canvas.height;
+
+	if (getCanvasContext()) {
 		var maxImaginary = minImaginary + (maxReal - minReal) * (height / width);
 		var realFactor = (maxReal - minReal) / (width - 1);
 		var imaginaryFactor = (maxImaginary - minImaginary) / (height - 1);
@@ -54,11 +102,11 @@ function drawMandelbrotWithWorkers(ctx, width, height, numWorkers) {
 		
 		var sliceForEachWorker = parseInt(height / numWorkers);
 		
-		// Start up the workers
+		// Send work to the workers
 		for (var i = 0; i < numWorkers; i++) {
 			startY = parseInt(i * sliceForEachWorker);
 
-			var imageData = ctx.createImageData(width, sliceForEachWorker);
+			var imageData = getCanvasContext().createImageData(width, sliceForEachWorker);
 			
 			var args = new Object;
 			args.workerID = i;
@@ -73,35 +121,21 @@ function drawMandelbrotWithWorkers(ctx, width, height, numWorkers) {
 			args.maxIterations = maxIterations;
 			args.imageData = imageData.data;
 
-			var worker = new Worker("mandelbrotRenderer.js");
-			
-			worker.onmessage = function(event) {
-				var theTime = new Date();
-
-				var renderValues = event.data;
-				var retWorkerID = renderValues.workerID;
-				var retStartX = renderValues.startX;
-				var retStartY = renderValues.startY;
-				var retImageData = renderValues.imageData;
-
-				logToMessageDiv("Worker " + retWorkerID + " returned from rendering: " + theTime + " diff: " + ((theTime - startTime) / 1000));
-
-				var newImageData = ctx.createImageData(width, sliceForEachWorker);
-				newImageData.data = retImageData;
-				ctx.putImageData(newImageData, retStartX, retStartY);
-
-				theTime = new Date();
-				logToMessageDiv("Worker " + retWorkerID + " finished drawing: " + theTime + " diff: " + ((theTime - startTime) / 1000));
-			};
-
-			worker.onerror = function(error) {
-				dump("Worker error: " + error.message + "\n");
-				throw error;
-			};
-
-			worker.postMessage(args);
+			workerPool[i].postMessage(args);
 		}
 	}
+}
+
+var ctx;
+function getCanvasContext() {
+	if (!ctx) {
+		var canvas = document.getElementById("can");
+		if (canvas.getContext) {
+			ctx = canvas.getContext("2d");
+		}
+	}	
+
+	return ctx;
 }
 
 var messageDiv;
